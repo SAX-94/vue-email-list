5437056a7b54883f715b70aa993e837bf779cb25
SAX_94 <matteosacchi94@gmail.com>
2022-06-17 16:54:35 +0200

    exercise completed

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        totalEmails: 10,
+        emailList: [],
+        generated: false,
+        errorMsg: "",
+        isError: false,
+    },
+    methods: {
+        generateEmailList() {
+            // La variabile "generated" viene comunque inizializzata a false nel caso in cui la funzione venisse richiamata altre volte tramite il pulsante.
+            this.generated = false;
+            this.emailList = [];
+            for (let i = 0; i < this.totalEmails; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then(resp => {
+                        this.emailList.push(resp.data.response);
+                        if (this.emailList.length === this.totalEmails) this.generated = true;
+                    })
+                    .catch(error => {
+                        this.errorMsg = error;
+                        this.isError = true;
+                    })
+            }
+        }
+    },
+    mounted() {
+        this.generateEmailList();
+    }
+});
